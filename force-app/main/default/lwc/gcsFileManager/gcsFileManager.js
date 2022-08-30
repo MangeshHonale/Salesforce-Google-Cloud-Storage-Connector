4f520d96bc99d754b8b072962958d34646d83ba7
Mangesh Honale <honalemangesh@gmail.com>
2022-08-29 22:41:28 -0400

    File storage path correction

--- force-app/main/default/lwc/gcsFileManager/gcsFileManager.js
+++ force-app/main/default/lwc/gcsFileManager/gcsFileManager.js
@@ -6,29 +6,21 @@ const resourceURL = 'https://storage.googleapis.com/storage/v1/b/';
 
 export default class GcsFileManager extends LightningElement {
     @api recordId;
-    _objectApiName;
-
-    renderedCallback(){
-        this.setupConnection();
-    }
 
     @wire(getRecord, { recordId: '$recordId', layoutTypes: ['Compact'] })
     getRecordDetails({ error, data }){
         if(data){
-            this._objectApiName = data.apiName;
+            let objectApiName = data.apiName;
             setupGoogleCloudStorageConnection().
             then(storageSetting => {
                 let fileList = this.template.querySelector('c-gcs-file-list');
-                fileList.loadFiles(storageSetting.bucketName, this._objectApiName, storageSetting.accessToken);
+                let storagePath = objectApiName + '/' + this.recordId;
+                fileList.loadFiles(storageSetting.bucketName, storagePath, storageSetting.accessToken);
 
                 let uploadFiles = this.template.querySelector('c-gcs-file-upload');
-                uploadFiles.initialize(storageSetting.bucketName, this._objectApiName, storageSetting.accessToken);
+                uploadFiles.initialize(storageSetting.bucketName, storagePath, storageSetting.accessToken);
 
             });
         }
-    }
-
-    setupConnection(){
-        
     }
 }
